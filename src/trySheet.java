dd7c37781e3ae2a8ff0ce01e53c7fdc15f1505a2
Artur Ciepielowski <artciepi@gmail.com>
2022-04-03 08:04:00 +0200

    More scanner code + still problems with gitignore

--- src/trySheet.java
+++ src/trySheet.java
@@ -11,9 +11,13 @@ public class trySheet {
         String name = scanner.nextLine();
         System.out.println("How old are you? ");
         int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is your favorite food?");
+        String food = scanner.nextLine();
 
         System.out.println("Hello "+name);
         System.out.println("You are "+age+" years old");
+        System.out.println("You like "+food);
     }
 
 }

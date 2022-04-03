70662d24a260b521dc72ed18de1dcf0861273338
Artur Ciepielowski <artciepi@gmail.com>
2022-04-03 08:10:05 +0200

    New branch new me! Still problems with gignore though

--- src/trySheet.java
+++ src/trySheet.java
@@ -2,22 +2,13 @@ import java.util.Scanner;
 
 public class trySheet {
 
-    public static void main(String[]args){
+    public static void main(String[]args) {
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What is your name? ");
+      int friends =10;
+      friends = friends +1;
 
-        String name = scanner.nextLine();
-        System.out.println("How old are you? ");
-        int age = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("What is your favorite food?");
-        String food = scanner.nextLine();
-
-        System.out.println("Hello "+name);
-        System.out.println("You are "+age+" years old");
-        System.out.println("You like "+food);
+      System.out.println(friends);
     }
 
 }

420ad2884713f8c98f25aab501f4ac8e8650e986
Artur Ciepielowski <artciepi@gmail.com>
2022-04-03 07:50:02 +0200

    Testing new gitignore

--- src/trySheet.java
+++ src/trySheet.java
@@ -9,8 +9,11 @@ public class trySheet {
         System.out.println("What is your name? ");
 
         String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
 
         System.out.println("Hello "+name);
+        System.out.println("You are "+age+" years old");
     }
 
 }

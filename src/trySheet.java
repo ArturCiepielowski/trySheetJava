467bd9cf988140d45d07a4f1ff78dddf8d5666e4
Artur Ciepielowski <artciepi@gmail.com>
2022-04-05 06:18:15 +0200

    GUI begining

--- src/trySheet.java
+++ src/trySheet.java
@@ -1,14 +1,11 @@
-import java.util.Scanner;
+
 
 public class trySheet {
 
     public static void main(String[]args) {
 
 
-      int friends =10;
-      friends = friends +1;
 
-      System.out.println(friends);
     }
 
 }

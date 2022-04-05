6a72ca543e7a4d04f26a2a43850bf4de8ee11cea
Artur Ciepielowski <artciepi@gmail.com>
2022-04-05 06:47:28 +0200

    Added one window

--- src/trySheet.java
+++ src/trySheet.java
@@ -1,10 +1,12 @@
+import javax.swing.JOptionPane;
 
-
+// TODO I need to create simple gui
 public class trySheet {
 
     public static void main(String[]args) {
 
-
+        String name= JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
 
     }
 

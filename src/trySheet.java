0ab64f7eb5373456212270d8dbac2c2ec5790e2e
Artur Ciepielowski <artciepi@gmail.com>
2022-04-06 06:39:06 +0200

    Added some years to gui

--- src/trySheet.java
+++ src/trySheet.java
@@ -8,6 +8,8 @@ public class trySheet {
         String name= JOptionPane.showInputDialog("Enter your name");
         JOptionPane.showMessageDialog(null, "Hello "+name);
 
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are "+age +" years old");
     }
 
 }

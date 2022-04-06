dd936e6d9136a0b640fa94ff723114d181baad2a
Artur Ciepielowski <artciepi@gmail.com>
2022-04-06 06:53:51 +0200

    First exercise with Math

--- src/trySheet.java
+++ src/trySheet.java
@@ -1,18 +1,11 @@
 import javax.swing.JOptionPane;
 
-// TODO I need to create simple gui
+
 public class trySheet {
 
     public static void main(String[]args) {
 
-        String name= JOptionPane.showInputDialog("Enter your name");
-        JOptionPane.showMessageDialog(null, "Hello "+name);
 
-        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-        JOptionPane.showMessageDialog(null, "You are "+age +" years old");
-
-        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
-        JOptionPane.showMessageDialog(null, "You are "+age +" cm tall");
     }
 
 }

c9c22aa180620adba3c064a73e190b95c8301baa
Artur Ciepielowski <artciepi@gmail.com>
2022-04-06 07:00:29 +0200

    Added some math calculations

--- src/trySheet.java
+++ src/trySheet.java
@@ -4,8 +4,18 @@ import javax.swing.JOptionPane;
 public class trySheet {
 
     public static void main(String[]args) {
+        double x =3.14;
+        double y = -10;
 
+        double z =Math.max(x, y);
 
+        double a =Math.min(x, y);
+
+        double b =Math.abs(y);
+
+        System.out.println(z);
+        System.out.println(a);
+        System.out.println(b);
     }
 
 }

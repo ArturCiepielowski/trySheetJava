28ff46e20ce8a3dda580475c1d62d29134bc0506
Artur Ciepielowski <artciepi@gmail.com>
2022-04-08 07:03:49 +0200

    Random begining

--- src/trySheet.java
+++ src/trySheet.java
@@ -1,39 +1,17 @@
 import javax.swing.JOptionPane;
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class trySheet {
 
     public static void main(String[]args) {
-        //double x =3.14;
-        //double y = -10;
 
-        //double z =Math.max(x, y);
+        Random random = new Random();
 
-        //double a =Math.min(x, y);
+        int x = random.nextInt();
+        System.out.println(x);
 
-        //double b =Math.abs(y);
-
-        //System.out.println(z);
-        //System.out.println(a);
-       // System.out.println(b);
-
-        double x;
-        double y;
-        double z;
-
-
-        Scanner scanner = new Scanner (System.in);
-
-        System.out.println("Enter side x: ");
-        x= scanner.nextDouble();
-        System.out.println("Enter side y: ");
-        y= scanner.nextDouble();
-        z=Math.sqrt((x*x)+(y*y));
-
-
-        System.out.println("The hypotenuse is :  " +z);
-        scanner.close();
     }
 
 }

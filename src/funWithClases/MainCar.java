10fe48d61499eb10556bae6584ce5592621a77d0
Artur Ciepielowski <artciepi@gmail.com>
2022-04-14 06:44:53 +0200

    try with methods

---
+++ src/funWithClases/MainCar.java
@@ -0,0 +1,12 @@
+package funWithClases;
+
+public class MainCar {
+    public static void main(String[] args) {
+
+        Car firstCar = new Car();
+
+        System.out.println(firstCar.make);
+        System.out.println(firstCar.model);
+
+    }
+}
--- src/trySheet.java
+++ src/trySheet.java
@@ -1,4 +1,5 @@
 import javax.swing.JOptionPane;
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -9,7 +10,15 @@ public class trySheet {
 
        String name ="Artur";
 
-       boolean result = name.equalsIgnoreCase("artur");
+       //boolean result = name.equalsIgnoreCase("artur");
+        //int result = name.length()
+       // char result = name.charAt(1);
+       // int result =name.indexOf("t");
+       // boolean result = name.isEmpty();
+        //String result = name.toUpperCase();
+        //String result = name.toLowerCase();
+        //String result = name.trim();
+        String result = name.replace('r','x');
        System.out.println(result);
 
     }
